ebc687c6cc6ded15a4d5329c34b6036bd2b9d76a
hemiao <hemiao@woyouqiu.com>
2018-05-01 21:54:24 +0800

    operator with from .

--- src/main.js
+++ src/main.js
@@ -1,33 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 
-import { Observable } from 'rxjs/Observable'
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/from';
 
-const fruitsObservable = Observable.create(function subscribe(observer) {
-    observer.next('🍎');
-    observer.next('🍊');
-    setTimeout(function () {
-        observer.next('🍋');
-        observer.complete();
-    }, 2000);
-})
-
-const fruitsObserver = {
-    next: function (data) {
-        console.log(data)
-    },
-    error: function (error) {
-        console.log(error)
-    },
-    complete: function () {
-        console.log('complete')
+const fruitsObservable = Observable.from(['🍎', '🍊', '🍋']);
+fruitsObservable.subscribe({
+    next: data => {
+        console.log(data);
     }
-}
-console.log('-----before subscribe ------ ');
-const fruitsSubscription = fruitsObservable.subscribe(fruitsObserver);
-
-console.log('-----after subscribe ------ ');
-
-setTimeout(function () {
-    fruitsSubscription.unsubscribe(); // didn't print '🍋'
-}, 1000);
+});
