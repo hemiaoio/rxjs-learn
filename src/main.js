97bbfa882b3ae1eba9e0cd25158f07d98ca84bee
hemiao <hemiao@woyouqiu.com>
2018-05-01 22:07:27 +0800

    operator with throttleTime!

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/from';
 import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/operator/throttleTime';
 
 const fruitsObservable = Observable.from(['🍎', '🍊', '🍋']);
 fruitsObservable.subscribe({
@@ -11,10 +12,25 @@ fruitsObservable.subscribe({
         console.log(data);
     }
 });
-
-const clickObservable = Observable.fromEvent(document.getElementById('search'), 'keyup');
+const clickObservable = Observable.fromEvent(document, 'click');
 clickObservable.subscribe({
+    next: data => {
+        console.log(data);
+    }
+});
+
+// const keyupObservable = Observable.fromEvent(document.getElementById('search'), 'keyup');
+// keyupObservable.subscribe({
+//     next: data => {
+//         console.log(data.key);
+//     }
+// });
+
+const searchObservable = Observable
+    .fromEvent(document.getElementById('search'), 'keyup')
+    .throttleTime(1000); // interval 1 second;
+searchObservable.subscribe({
     next: data => {
         console.log(data.key);
     }
-})
+});
