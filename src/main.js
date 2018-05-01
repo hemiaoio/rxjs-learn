7aff40ed01c2f6b407118dbe72021da692172b3c
hemiao <hemiao@woyouqiu.com>
2018-05-01 22:21:49 +0800

    merge before code.

--- src/main.js
+++ src/main.js
@@ -7,8 +7,37 @@ import 'rxjs/add/observable/fromEvent';
 import 'rxjs/add/operator/throttleTime';
 import 'rxjs/add/operator/debounceTime';
 
-const fruitsObservable = Observable.from(['🍎', '🍊', '🍋']);
-fruitsObservable.subscribe({
+const fruitsObservable = Observable.create(function subscribe(observer) {
+    observer.next('🍎');
+    observer.next('🍊');
+    setTimeout(function () {
+        observer.next('🍋');
+        observer.complete();
+    }, 2000);
+})
+
+const fruitsObserver = {
+    next: function (data) {
+        console.log(data)
+    },
+    error: function (error) {
+        console.log(error)
+    },
+    complete: function () {
+        console.log('complete')
+    }
+}
+console.log('-----before subscribe ------ ');
+const fruitsSubscription = fruitsObservable.subscribe(fruitsObserver);
+
+console.log('-----after subscribe ------ ');
+
+setTimeout(function () {
+    fruitsSubscription.unsubscribe(); // didn't print '🍋'
+}, 1000);
+
+const fruitsObservableWithFrom = Observable.from(['🍎', '🍊', '🍋']);
+fruitsObservableWithFrom.subscribe({
     next: data => {
         console.log(data);
     }
